5c5e03a06494d6a4813c05d4193cead71e7918cd
Shama Garg <shama.aries@gmail.com>
2025-10-28 19:56:54 +0530

    new changes

--- src/test/java/package2/DemoTest.java
+++ src/test/java/package2/DemoTest.java
@@ -8,7 +8,7 @@ public class DemoTest {
 	
 	public void mangoTest()
 	{
-		System.out.println("workplace1- package2 changes- mango tested");
+		System.out.println("workplace1- package2 changes- new mango tested");
 	}
 
 }
